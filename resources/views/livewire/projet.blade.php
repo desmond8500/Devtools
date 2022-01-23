1dae8aff5e643e5775f116915b3754c0da971d2c
kratos <desmond8500@gmail.com>
2022-01-23 12:49:01 +0000

    fix

--- resources/views/livewire/projet.blade.php
+++ resources/views/livewire/projet.blade.php
@@ -25,7 +25,7 @@
             <!-- Page title actions -->
             <div class="col-auto ms-auto d-print-none">
                 <div class="d-flex btn-list">
-                    @if ($tab_selected==2)
+                    @if ($tab_selected==0)
                         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalActeur">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
                                 stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -35,6 +35,17 @@
                             </svg>
                             Acteur
                         </button>
+
+                        <a href="#" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#modalTeam">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <line x1="12" y1="5" x2="12" y2="19" />
+                                <line x1="5" y1="12" x2="19" y2="12" />
+                            </svg>
+                            Team
+                        </a>
+                    @elseif($tab_selected==2)
                         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#fonctionalite">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24" height="24"
                                 viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
@@ -45,18 +56,8 @@
                             </svg>
                             Fonctionalité
                         </button>
-                        <a href="#" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#modalTeam">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
-                                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <line x1="12" y1="5" x2="12" y2="19" />
-                                <line x1="5" y1="12" x2="19" y2="12" />
-                            </svg>
-                            Team
-                        </a>
-                    </div>
-
                     @endif
+                </div>
             </div>
         </div>
     </div>
